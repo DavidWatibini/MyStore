189ead1be4fa5b28e2991a424de60424cce23338
DavidWatibini <watimakhanu@gmail.com>
2024-10-14 16:47:27 +0300

    Load CSV Files

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ def load_csv_groceries(file_path):
 
     groceries = []  # I Initialize an empty list to store new grocery data
     
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile) # Read the CSV file into a dictionary reader
-        for row in reader:
+    with open(file_path, mode='r', newline='') as csvfile:
+        storeCsv = csv.DictReader(csvfile) # Read the CSV file into a dictionary reader
+        for row in storeCsv:
             # Create a dictionary for the grocery item and convert it to types needed
             grocery_item = { 
+
                 'grocery_id': int(row['grocery_id']), 
                 'grocery_name': row['grocery_name'],
                 'price': float(row['price']),  
@@ -42,15 +43,35 @@ def load_csv_transactions(file_path):
 
     transactions = [] # I Initialize an empty list to store new transaction data
     
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
+    with open(file_path, mode='r', newline='') as csvfile:
+        storeCsv = csv.DictReader(csvfile)
+        for row in storeCsv:
             # Append each transaction to the list
             transactions.append(row)
 
     return transactions 
 
     
+#Save new imput grocery products to the CSV file.
+
+def save_groceries_csv(file_path, groceries):
+
+    with open(file_path, mode='w', newline='') as csvfile:
+         storeCsv = csv.writer(csvfile)
+        storeCsv.writerow(['id', 'name', 'price', 'stock'])
+
+        for grocery_id, data in groceries.items():
+            # Write each grocery item as a row in the CSV
+            storeCsv.writerow([grocery_id, data['name'], data['price'], data['stock']])
 
 
+#Save sales transactions to the CSV file.
+
+def save_transactions_csv(file_path, transactions):
+    with open(file_path, mode='w', newline='') as csvfile:
+        
+        storeCsv = csv.writer(csvfile)
+        storeCsv.writerow(['date_time', 'grocery_id', 'quantity', 'payment']) 
+        for transaction in transactions:
+            storeCsv.writerow([transaction['date_time'], transaction['grocery_id'], transaction['quantity'], transaction['payment']])
 
